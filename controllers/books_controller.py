8006d51102dc0fe6a150fcdb15e0d245291eb195
Char Broadley <broadleychar@gmail.com>
2022-12-14 16:57:07 +0000

    finished mvp

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -9,7 +9,7 @@ books_blueprint = Blueprint("books", __name__)
 @books_blueprint.route("/books")
 def books():
     books = book_repository.select_all()
-    return render_template("books/index.html", all_books = books)
+    return render_template("index.html", all_books = books)
 
 @books_blueprint.route("/books/<id>/delete", methods = ['POST'])
 def delete_book (id):
